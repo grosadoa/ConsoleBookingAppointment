4df812059e31dd94b222c2d913d6233f39b3ecaf
freddy rosemberg <freddy.rosember.jeff@gmail.com>
2023-10-21 13:16:07 -0500

    add project test

---
+++ TestTickets/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestTickets
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
