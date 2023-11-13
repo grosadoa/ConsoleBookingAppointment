1a35303c25f3f146c894e1f0c1a96f3acdc28609
freddy rosemberg <freddy.rosember.jeff@gmail.com>
2023-11-13 00:17:31 -0500

    delete files

--- BussinessTickets/RepositorySystem.cs
+++ BussinessTickets/RepositorySystem.cs
@@ -10,8 +10,6 @@ namespace BussinessBookingAppointment
 {
     public class RepositorySystem
     {
-        public static List<Events> lEvents { get; set; } = new List<Events>();
-        public static List<PriceByTicket> lPriceByTicket { get; set; } = new List<PriceByTicket>();
 
         public static List<DateTime> datesHoliday { get; set; } = new List<DateTime> { DateTime.Parse("2023/11/20"),DateTime.Parse("2023/11/13") };
 
